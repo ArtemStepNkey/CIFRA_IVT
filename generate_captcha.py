95af903ada8983506d78e2213222dc31f5231e76
AlafurZerk <kapowew@yandex.ru>
2024-10-21 23:09:38 +0300

    first commit

---
+++ generate_captcha.py
@@ -0,0 +1,27 @@
+from captcha.image import ImageCaptcha
+import random
+import string
+import base64
+import io
+
+
+def generate_captcha_text(length=6):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+
+
+def generate_captcha_image(text):
+    image_captcha = ImageCaptcha(width=200, height=70)
+
+    image = image_captcha.generate_image(text)
+
+    buffered = io.BytesIO()
+    image.save(buffered, format="PNG")
+
+    img_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+    return img_base64
+
+
+captcha_text = generate_captcha_text()
+captcha_image_base64 = generate_captcha_image(captcha_text)
+print(f"Captcha Image (Base64): {captcha_image_base64}")
---
+++ generate_token.py
@@ -0,0 +1,40 @@
+import time
+
+from flask import Flask
+from flask_jwt_extended import create_access_token, JWTManager, decode_token
+import uuid
+import os
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'KEY'  # Установите секретный ключ для JWT
+jwt = JWTManager(app)
+
+load_dotenv()
+
+authorization_limit = int(os.getenv("AUTH_MIN_SESSION")) * 60
+
+def create_uuid():
+    return str(uuid.uuid4())
+
+def create_user_token(login: str, password: str) -> str:
+    current_time = datetime.now()
+    expiration_time = current_time + timedelta(seconds=authorization_limit)
+
+    token = create_access_token(identity={
+        "created_time": int(current_time.timestamp()),
+        "login": login,
+        "password": password,
+        "expected_time": int(expiration_time.timestamp())
+    })
+
+    return token
+
+
+with app.app_context():
+    token = create_user_token("login", "password")
+    print("Encoded Token:", token)
+
+    jwt_decoded_token = decode_token(token)
+    print("JWT Decoded Token:", jwt_decoded_token)
